0adf34357f9709d4760721540cb643b934ccd06a
aditya9277 <adisamarth20@gmail.com>
2024-12-16 00:35:59 +0530

    v1

--- src/server.js
+++ src/server.js
@@ -2,21 +2,16 @@ const express = require('express');
 const routes = require('./routes');
 const errorHandler = require('./middleware/errorHandler');
 const logger = require('./utils/logger');
-
-
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-// Routes
 app.use('/api', routes);
 
-// Error handling
 app.use(errorHandler);
 
-app.listen(PORT,'0.0.0.0', () => {
+app.listen(PORT, () => {
   logger.info(`Server running on port ${PORT}`);
 });
 
