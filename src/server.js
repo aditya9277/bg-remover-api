b9ec01fa93e0e03fdbe639c144263da3be139b25
aditya9277 <adisamarth20@gmail.com>
2024-12-16 01:43:12 +0530

    v5

--- src/server.js
+++ src/server.js
@@ -2,12 +2,14 @@ const express = require('express');
 const routes = require('./routes');
 const errorHandler = require('./middleware/errorHandler');
 const logger = require('./utils/logger');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
 app.use('/api', routes);
+app.use('/images', express.static(path.join('/tmp', 'processed-images')));
 
 app.use(errorHandler);
 
